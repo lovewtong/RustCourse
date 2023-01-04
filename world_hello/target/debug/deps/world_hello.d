a2756b07eea506b0c56c72224e731b7762c45004
lovewtong <suqin2106@gmail.com>
2023-01-04 22:11:23 +0800

    demo

---
+++ world_hello/target/debug/deps/world_hello.d
@@ -0,0 +1,5 @@
+C:\Users\45940\Desktop\RustCourse\world_hello\target\debug\deps\world_hello.exe: src\main.rs
+
+C:\Users\45940\Desktop\RustCourse\world_hello\target\debug\deps\world_hello.d: src\main.rs
+
+src\main.rs:
---
+++ world_hello/target/debug/world_hello.d
@@ -0,0 +1,1 @@
+C:\Users\45940\Desktop\RustCourse\world_hello\target\debug\world_hello.exe: C:\Users\45940\Desktop\RustCourse\world_hello\src\main.rs
